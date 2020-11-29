98119cea2c77a17dfc876f9567da64357c565824
Nikita Gerasimov <33506510+Nikel163@users.noreply.github.com>
2020-11-30 00:02:46 +0400

    Update EntityService.java

--- src/main/java/service/EntityService.java
+++ src/main/java/service/EntityService.java
@@ -21,50 +21,10 @@ public abstract class EntityService<T> {
         return result;
     }
 
-    public T findById(Integer id) {
-        EntityManager em = entityManagerFactory.createEntityManager();
-        T result = em.find(cl, id);
-        em.close();
-        return result;
-    }
-
     public void create(T entity) {
         EntityManager em = entityManagerFactory.createEntityManager();
         em.getTransaction().begin();
         em.persist(entity);
-        em.getTransaction().commit();
-        em.close();
-    }
-
-    public T update(T entity) {
-        EntityManager em = entityManagerFactory.createEntityManager();
-        em.getTransaction().begin();
-        T result = em.merge(entity);
-        em.getTransaction().commit();
-        em.close();
-        return result;
-    }
-
-    public void delete(T entity) {
-        EntityManager em = entityManagerFactory.createEntityManager();
-        if (!em.contains(entity)) {
-            return;
-        }
-        em.getTransaction().begin();
-        em.remove(entity);
-        em.getTransaction().commit();
-        em.close();
-    }
-
-    public void deleteById(Integer id) {
-        T entity = findById(id);
-        delete(entity);
-    }
-
-    public void createNativeQuery(String query) {
-        EntityManager em = entityManagerFactory.createEntityManager();
-        em.getTransaction().begin();
-        em.createNativeQuery(query).executeUpdate();
         em.getTransaction().commit();
         em.close();
     }
