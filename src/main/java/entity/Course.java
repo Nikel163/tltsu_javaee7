b8af8358f44d7d0f10b773e3304841078c47fcc7
Nikita Gerasimov <33506510+Nikel163@users.noreply.github.com>
2020-11-30 00:03:09 +0400

    Update Course.java

--- src/main/java/entity/Course.java
+++ src/main/java/entity/Course.java
@@ -9,7 +9,7 @@ import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Set;
 
-@Entity(name = "Course")
+@Entity
 @Data
 @Table(name = "courses")
 @NoArgsConstructor
